af1aa5e250fd1ccf681ab1379eca0e1354133df7
Alexander Landmesser <alexander@landmesser.online>
2025-05-08 19:59:55 +0200

    Add binary sensor for events

--- custom_components/onlycat/binary_sensor.py
+++ custom_components/onlycat/binary_sensor.py
@@ -2,35 +2,16 @@
 
 from __future__ import annotations
 
-import logging
 from typing import TYPE_CHECKING
 
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-    BinarySensorEntityDescription,
-)
-from homeassistant.const import EntityCategory
-from homeassistant.helpers.device_registry import DeviceInfo
-
-from .const import DOMAIN
-
-_LOGGER = logging.getLogger(__name__)
-
+from .binary_sensor_connectivity import OnlyCatConnectionSensor
+from .binary_sensor_event import OnlyCatEventSensor
 
 if TYPE_CHECKING:
     from homeassistant.core import HomeAssistant
     from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-    from .api import OnlyCatApiClient
     from .data import OnlyCatConfigEntry
-
-
-ENTITY_DESCRIPTION = BinarySensorEntityDescription(
-    key="OnlyCat",
-    name="OnlyCat Flap",
-    device_class=BinarySensorDeviceClass.CONNECTIVITY,
-)
 
 
 async def async_setup_entry(
@@ -40,58 +21,16 @@ async def async_setup_entry(
 ) -> None:
     """Set up the sensor platform."""
     async_add_entities(
-        OnlyCatConnectionSensor(
-            device=device,
-            entity_description=ENTITY_DESCRIPTION,
-            api_client=entry.runtime_data.client,
-        )
+        sensor
         for device in entry.data["devices"]
+        for sensor in (
+            OnlyCatEventSensor(
+                device=device,
+                api_client=entry.runtime_data.client,
+            ),
+            OnlyCatConnectionSensor(
+                device=device,
+                api_client=entry.runtime_data.client,
+            ),
+        )
     )
-
-
-class OnlyCatConnectionSensor(BinarySensorEntity):
-    """OnlyCat Sensor class."""
-
-    _attr_has_entity_name = True
-    _attr_should_poll = False
-    _attr_device_class = BinarySensorDeviceClass.CONNECTIVITY
-    entity_category = EntityCategory.DIAGNOSTIC
-    _attr_translation_key = "onlycat_connection_sensor"
-
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return device info to map to a device."""
-        return DeviceInfo(
-            identifiers={(DOMAIN, self.device["deviceId"])},
-            name=self.device["description"],
-            serial_number=self.device["deviceId"],
-        )
-
-    def __init__(
-        self,
-        device: dict,
-        entity_description: BinarySensorEntityDescription,
-        api_client: OnlyCatApiClient,
-    ) -> None:
-        """Initialize the sensor class."""
-        self.entity_description = entity_description
-        self._state = None
-        self._attr_raw_data = None
-        self.device = device
-        self._attr_name = "Connectivity"
-        self._attr_unique_id = (
-            device["deviceId"].replace("-", "_").lower() + "_connectivity"
-        )
-        self.entity_id = "binary_sensor." + self._attr_unique_id
-        api_client.add_event_listener("deviceUpdate", self.on_device_update)
-
-    async def on_device_update(self, data: dict) -> None:
-        """Handle device update event."""
-        _LOGGER.debug("Device update event received for binary sensor: %s", data)
-        self._attr_raw_data = str(data)
-        self.async_write_ha_state()
-
-    @property
-    def is_on(self) -> bool:
-        """Return if device is connected."""
-        return self.device["connectivity"]["connected"]
---
+++ custom_components/onlycat/binary_sensor_event.py
@@ -0,0 +1,108 @@
+"""Sensor platform for OnlyCat."""
+
+from __future__ import annotations
+
+import logging
+from typing import TYPE_CHECKING
+
+from homeassistant.components.binary_sensor import (
+    BinarySensorDeviceClass,
+    BinarySensorEntity,
+    BinarySensorEntityDescription,
+)
+from homeassistant.const import EntityCategory
+from homeassistant.helpers.device_registry import DeviceInfo
+
+from .const import DOMAIN
+
+_LOGGER = logging.getLogger(__name__)
+
+if TYPE_CHECKING:
+    from homeassistant.core import HomeAssistant
+    from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+    from .api import OnlyCatApiClient
+    from .data import OnlyCatConfigEntry
+
+
+ENTITY_DESCRIPTION = BinarySensorEntityDescription(
+    key="OnlyCat",
+    name="OnlyCat Flap",
+    device_class=BinarySensorDeviceClass.MOTION,
+)
+
+
+class OnlyCatEventSensor(BinarySensorEntity):
+    """OnlyCat Sensor class."""
+
+    _attr_has_entity_name = True
+    _attr_should_poll = False
+    _attr_device_class = BinarySensorDeviceClass.MOTION
+    entity_category = EntityCategory.DIAGNOSTIC
+    _attr_translation_key = "onlycat_event_sensor"
+
+    @property
+    def device_info(self) -> DeviceInfo:
+        """Return device info to map to a device."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, self.device["deviceId"])},
+            name=self.device["description"],
+            serial_number=self.device["deviceId"],
+        )
+
+    def __init__(
+        self,
+        device: dict,
+        api_client: OnlyCatApiClient,
+    ) -> None:
+        """Initialize the sensor class."""
+        self.entity_description = ENTITY_DESCRIPTION
+        self._state = False
+        self._attr_raw_data = None
+        self.device = device
+        self._attr_name = "Flap event"
+        self._attr_unique_id = device["deviceId"].replace("-", "_").lower() + "_event"
+        self._api_client = api_client
+        self.entity_id = "sensor." + self._attr_unique_id
+        api_client.add_event_listener("deviceEventUpdate", self.on_device_event_update)
+        api_client.add_event_listener("eventUpdate", self.on_event_update)
+
+    async def on_event_update(self, data: dict) -> None:
+        """Handle event update event."""
+        _LOGGER.debug("Event update event received for sensor: %s", data)
+        if (self._attr_extra_state_attributes["eventId"]) != data["eventId"]:
+            _LOGGER.debug("Event ID has changed, updating state.")
+            self._state = True
+        else:
+            if "frameCount" in data["body"] and data["body"]["frameCount"] > 0:
+                self._state = False
+            if "rfidCodes" in data["body"] and len(data["body"]["rfidCodes"]) > 0:
+                self._attr_extra_state_attributes["rfidCodes"] = data["body"][
+                    "rfidCodes"
+                ]
+        self.async_write_ha_state()
+
+    async def on_device_event_update(self, data: dict) -> None:
+        """Handle device event update event."""
+        _LOGGER.debug("Device event update event received for binary sensor: %s", data)
+        response = await self._api_client.send_message(
+            "getEvent",
+            {
+                "deviceId": data["deviceId"],
+                "eventId": data["eventId"],
+                "subscribe": True,
+            },
+        )
+        _LOGGER.debug("Response from getEvent: %s", response)
+        self._state = True
+        self._attr_extra_state_attributes = {
+            "eventId": response["eventId"],
+            "timestamp": response["timestamp"],
+            "eventTriggerSource": response["eventTriggerSource"],
+        }
+        self.async_write_ha_state()
+
+    @property
+    def is_on(self) -> bool:
+        """Return if device is connected."""
+        return self._state
