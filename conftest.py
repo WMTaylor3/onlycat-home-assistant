efb698092589b618bdb049328cf57e46c2c42f39
Alexander Landmesser <alexander@landmesser.online>
2025-08-02 15:06:24 +0200

    fix lint

--- conftest.py
+++ conftest.py
@@ -1,5 +1,7 @@
-import os
+"""Configuration of pytest."""
+
 import sys
+from pathlib import Path
 
 # Add the root directory to Python path so custom_components can be found
-sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+sys.path.insert(0, str(Path(Path(__file__).parent).resolve()))
--- custom_components/onlycat/tests/test_init.py
+++ custom_components/onlycat/tests/test_init.py
@@ -1,117 +1,120 @@
+"""Tests for OnlyCat/__init.py."""
+
+from typing import Any
 from unittest.mock import AsyncMock, patch
+
 import pytest
-from typing import TYPE_CHECKING, Any
 
-from custom_components.onlycat import _initialize_devices, _retrieve_current_transit_policy
-from custom_components.onlycat.data.device import Device
-from custom_components.onlycat.data.policy import DeviceTransitPolicy
-from custom_components.onlycat.api import OnlyCatApiClient
+from custom_components.onlycat import (
+    _initialize_devices,
+)
 
-getDevices = [
+get_devices = [
     # "Normal device"
     {
-        "deviceId":"OC-00000000001",
-        "description":"Device Name",
-        "timeZone":"Europe/Zurich",
-        "deviceTransitPolicyId":0000,
-        "cursorId":0000
+        "deviceId": "OC-00000000001",
+        "description": "Device Name",
+        "timeZone": "Europe/Zurich",
+        "deviceTransitPolicyId": 0000,
+        "cursorId": 0000,
     },
     # Fresh device with no transit policy
     {
-        "deviceId":"OC-00000000002",
-        "description":"Device Name",
-        "timeZone":"Europe/Zurich",
-        "cursorId":0000
-    }
+        "deviceId": "OC-00000000002",
+        "description": "Device Name",
+        "timeZone": "Europe/Zurich",
+        "cursorId": 0000,
+    },
 ]
-getDevice = { 
+get_device = {
     "OC-00000000001": {
-        "deviceId":"OC-00000000001",
-        "description":"Durins Tor",
-        "timeZone":"Europe/Zurich",
-        "deviceTransitPolicyId":0000,
+        "deviceId": "OC-00000000001",
+        "description": "Durins Tor",
+        "timeZone": "Europe/Zurich",
+        "deviceTransitPolicyId": 0000,
         "connectivity": {
             "connected": True,
             "disconnectReason": None,
-            "timestamp":1743841488269
-        }
+            "timestamp": 1743841488269,
+        },
     },
     "OC-00000000002": {
-        "deviceId":"OC-00000000002",
-        "description":"Durins Tor",
-        "timeZone":"Europe/Zurich",
+        "deviceId": "OC-00000000002",
+        "description": "Durins Tor",
+        "timeZone": "Europe/Zurich",
         "connectivity": {
             "connected": True,
             "disconnectReason": None,
-            "timestamp":1743841488269
-        }
-    }
+            "timestamp": 1743841488269,
+        },
+    },
 }
-getDeviceTransitPolicy = {
+get_device_transit_policy = {
     0: {
-        "deviceTransitPolicyId":0000,
-        "deviceId":"OC-00000000001",
-        "name":"Nachts",
+        "deviceTransitPolicyId": 0000,
+        "deviceId": "OC-00000000001",
+        "name": "Nachts",
         "transitPolicy": {
             "rules": [
                 {
-                    "action":{
-                        "lock": True
+                    "action": {"lock": True},
+                    "criteria": {
+                        "eventTriggerSource": 3,
+                        "eventClassification": [2, 3],
                     },
-                    "criteria":{
-                        "eventTriggerSource":3,
-                        "eventClassification":[2,3]
-                    },
-                    "description":"Contraband Rule"
+                    "description": "Contraband Rule",
                 },
                 {
-                    "action":{
-                        "lock": False
-                    },
+                    "action": {"lock": False},
                     "enabled": True,
-                    "criteria":{
-                        "rfidCode":[
+                    "criteria": {
+                        "rfidCode": [
                             "000000000000003",
                             "000000000000001",
-                            "000000000000002"
+                            "000000000000002",
                         ],
-                        "eventTriggerSource":3
+                        "eventTriggerSource": 3,
                     },
-                    "description":"Entry Rule"
-                }
+                    "description": "Entry Rule",
+                },
             ],
             "idleLock": True,
-            "idleLockBattery":True
-        }
+            "idleLockBattery": True,
+        },
     }
 }
 
-# Setup test device api responses
+
 async def mock_send_message(topic: str, data: dict) -> Any | None:
-    if topic == "getDevices": 
-        return getDevices
+    """Mock of OnlyCatApiClient.send_message to return test api responses."""
+    if topic == "getDevices":
+        return get_devices
     if topic == "getDevice":
-        return getDevice[data["deviceId"]]
+        return get_device[data["deviceId"]]
     if topic == "getDeviceTransitPolicy":
-        return getDeviceTransitPolicy[data["deviceTransitPolicyId"]]
+        return get_device_transit_policy[data["deviceTransitPolicyId"]]
+    return None
 
 
 @pytest.mark.asyncio
-async def test_initialize_devices():
+async def test_initialize_devices() -> None:
+    """Test _initialize_devices."""
     mock_entry = AsyncMock()
     mock_entry.runtime_data.devices = []
     mock_entry.runtime_data.client = AsyncMock()
     mock_entry.runtime_data.client.send_message.side_effect = mock_send_message
-    with patch("custom_components.onlycat._retrieve_current_transit_policy") as mock_retrieve_current_transit_policy:
+    with patch(
+        "custom_components.onlycat._retrieve_current_transit_policy"
+    ) as mock_retrieve_current_transit_policy:
         await _initialize_devices(mock_entry)
 
-    assert len(mock_entry.runtime_data.devices) == len(getDevices)
+    assert len(mock_entry.runtime_data.devices) == len(get_devices)
     mock_entry.runtime_data.client.send_message.assert_any_call(
         "getDevices", {"subscribe": True}
     )
-    for device_id in getDevice:
+    for device_id in get_device:
         mock_entry.runtime_data.client.send_message.assert_any_call(
             "getDevice", {"deviceId": device_id, "subscribe": True}
         )
-    assert mock_entry.runtime_data.devices[1].device_transit_policy_id == None
+    assert mock_entry.runtime_data.devices[1].device_transit_policy_id is None
     assert mock_retrieve_current_transit_policy.call_count == 1
